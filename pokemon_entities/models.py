5354635f13787a9fb2eedfc1b413e4f21ee99bca
Orlov Oleg (Russian ICS) <o84083853@china.huawei.com>
2019-08-03 15:00:17 +0300

    Fix dvmn remarks to models

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -1,12 +1,13 @@
 from django.db import models
 
 class Pokemon(models.Model):
-    title_ru = models.CharField(max_length=200)
-    title_en = models.CharField(max_length=200, blank=True)
-    title_jp = models.CharField(max_length=200, blank=True)
-    description = models.TextField(blank=True)
+    title_ru = models.CharField(max_length=200, verbose_name='Имя на русском')
+    title_en = models.CharField(max_length=200, verbose_name='Имя на английском', blank=True)
+    title_jp = models.CharField(max_length=200, verbose_name='Имя на японском', blank=True)
+    description = models.TextField(blank=True, verbose_name='Описание')
     next_evolution = models.OneToOneField(
         "Pokemon", related_name='previous_evolution', 
+        verbose_name='Описание',
         on_delete=models.SET_NULL, null=True, blank=True
         )
     image = models.ImageField(upload_to='pokemons', blank=True)
@@ -16,16 +17,16 @@ class Pokemon(models.Model):
 
 
 class PokemonEntity(models.Model):
-    pokemon = models.ForeignKey(Pokemon, on_delete=models.CASCADE)
-    latitude = models.FloatField()
-    longitude = models.FloatField()
-    appeared_at = models.DateTimeField(default=None)
-    dissapeared_at = models.DateTimeField(default=None)
-    level = models.IntegerField(null=True, blank=True)
-    health = models.IntegerField(null=True, blank=True)
-    strength = models.IntegerField(null=True, blank=True)
-    defence = models.IntegerField(null=True, blank=True)
-    stamina = models.IntegerField(null=True, blank=True)
+    pokemon = models.ForeignKey(Pokemon, verbose_name='Покемон', on_delete=models.CASCADE)
+    latitude = models.FloatField(verbose_name='Широта')
+    longitude = models.FloatField(verbose_name='Долгота')
+    appeared_at = models.DateTimeField(verbose_name='Время появления', null=True)
+    dissapeared_at = models.DateTimeField(verbose_name='Время исчезновения', null=True)
+    level = models.IntegerField(verbose_name='Уровень', null=True, blank=True)
+    health = models.IntegerField(verbose_name='Здоровье', null=True, blank=True)
+    strength = models.IntegerField(verbose_name='Сила', null=True, blank=True)
+    defence = models.IntegerField(verbose_name='Защита', null=True, blank=True)
+    stamina = models.IntegerField(verbose_name='Стамина', null=True, blank=True)
 
     def __str__(self):
         return "Pokemon {}_{} level {}".format(self.pokemon, self.id, self.level)
